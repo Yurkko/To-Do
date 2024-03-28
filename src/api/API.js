cd7efb792a63f67cb0f226d4fb31adc816657eea
Yura Serkiz <serkiz1040@gmail.com>
2024-03-29 00:31:29 +0200

    Change Port

--- src/api/API.js
+++ src/api/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:4000/';
+axios.defaults.baseURL = 'http://localhost:3000/';
 
 export const fetchToDoList = async () => {
    const response = await axios.get('todos');
